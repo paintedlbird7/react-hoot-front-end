428a06ef8601fad5234276d30def7aea570b586f
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2025-04-01 10:13:32 -0700

    built the hootlist route

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,37 @@ import SignUpForm from './components/SignUpForm/SignUpForm';
 import SignInForm from './components/SignInForm/SignInForm';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
+import HootList from './components/HootList/HootList';
 
 import { UserContext } from './contexts/UserContext';
 
 const App = () => {
   const { user } = useContext(UserContext);
   
-  return (
-    <>
-      <NavBar/>
-      <Routes>
-        <Route path='/' element={user ? <Dashboard /> : <Landing />} />
-        <Route path='/sign-up' element={<SignUpForm />} />
-        <Route path='/sign-in' element={<SignInForm />} />
-      </Routes>
-    </>
-  );
+ // src/App.jsx
+
+ return (
+  <>
+    <NavBar/>
+    <Routes>
+      <Route path='/' element={user ? <Dashboard /> : <Landing />} />
+      {user ? (
+        <>
+          {/* Protected routes (available only to signed-in users) */}
+          <Route path='/hoots' element={<HootList />} />
+        </>
+      ) : (
+        <>
+          {/* Non-user routes (available only to guests) */}
+          <Route path='/sign-up' element={<SignUpForm />} />
+          <Route path='/sign-in' element={<SignInForm />} />
+        </>
+      )}
+    </Routes>
+  </>
+);
+
 };
 
 export default App;
+// TODO: Create hootService.js
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -11,23 +11,25 @@ const NavBar = () => {
     setUser(null);
   };
 
-  return (
-    <nav>
-      {user ? (
-        <ul>
-          <li>Welcome, {user.username}</li>
-          <li><Link to='/'>Dashboard</Link></li>
-          <li><Link to='/' onClick={handleSignOut}>Sign Out</Link></li>
-        </ul>
-      ) : (
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/sign-in'>Sign In</Link></li>
-          <li><Link to='/sign-up'>Sign Up</Link></li>
-        </ul>
-      )}
-    </nav>
-  );
+return (
+  <nav>
+    {user ? (
+      <ul>
+        <li><Link to='/'>HOME</Link></li>
+        <li><Link to='/hoots'>HOOTS</Link></li>
+        <li><Link to='/' onClick={handleSignOut}>Sign Out</Link></li>
+      </ul>
+    ) : (
+      <ul>
+        <li><Link to='/'>HOME</Link></li>
+        <li><Link to='/sign-in'>SIGN IN</Link></li>
+        <li><Link to='/sign-up'>SIGN UP</Link></li>
+      </ul>
+    )}
+  </nav>
+);
+
+
 };
 
 export default NavBar;

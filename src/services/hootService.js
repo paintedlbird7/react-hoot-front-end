5c2591cf1886e3f98518cf26d2c8ce3b86f2df33
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2025-04-01 10:51:45 -0700

    hoots links to another route

--- src/services/hootService.js
+++ src/services/hootService.js
@@ -1,6 +1,7 @@
 // src/services/hootService.js
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/hoots`;
 // TODO: do User stories for trello AAU, under component hierarchy diagram
+// TODO: replace the existing <p> tags with clickable links
 
 const index = async () => {
     try {

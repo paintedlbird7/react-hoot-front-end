6f94d9142b5b1b5c407add6a022fe4801ced365a
Rosa Perez <rosaperez@Rosas-MacBook-Pro.local>
2025-04-01 14:20:55 -0700

    completed CRUD

--- src/services/hootService.js
+++ src/services/hootService.js
@@ -1,7 +1,7 @@
 // src/services/hootService.js
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/hoots`;
 // TODO: do User stories for trello AAU, under component hierarchy diagram
-// TODO: Back in the HootForm component, update the existinghandleSubmit() function to use this new function when we’re updating an existing hoot:
+// TODO:
 
 
 
